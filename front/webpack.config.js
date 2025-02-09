61a9526fa094d4b2a92b766d3af22d9ee02e80c4
David Natan <rdpalominop@ucvvirtual.edu.pe>
2025-02-09 17:49:56 +0100

    fixed vercerl 5

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,17 +1,7 @@
-const path = require("path");
-
 module.exports = {
-  entry: "./scripts/index.js",
-  output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
+    entry: "./scripts/index.js",
+    output: {
+        path: __dirname + "/public",
+        filename: "bundle.js",
+    },
 };
